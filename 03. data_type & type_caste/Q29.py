842d97193a8283fc9e1590c186ff2d0302b81701
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-24 23:30:52 -0400

    Q29 is solved

---
+++ 03. data_type & type_caste/Q29.py
@@ -0,0 +1,19 @@
+product_name="Bag"
+product_quantity=4
+product_price=99.99
+product_available=True
+product_discount=None
+
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(product_available))
+print(type(product_discount))
+
+#the output is
+#<class 'str'>
+#class 'int'>
+#<class 'float'>
+#<class 'bool'>
+#<class 'NoneType'>
+
