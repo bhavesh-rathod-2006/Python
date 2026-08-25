b0a6d415404f8ca45af5edd82fa23f3014a8e2f3
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-24 23:36:25 -0400

    Q30 is soled

---
+++ 03. data_type & type_caste/Q30.py
@@ -0,0 +1,21 @@
+age=10
+price=10.0
+value="10"
+available=True
+text="True"
+discount=None
+source="None"
+
+
+print(type(age))
+print(type(price))
+print(type(value))
+print(type(available))
+print(type(text))
+print(type(discount))
+print(type(source))
+
+#the output is:-
+
+
+
