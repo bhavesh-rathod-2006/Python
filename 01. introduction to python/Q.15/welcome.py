6bb3d3432db9b2373965dbfb5bba0226600d9421
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-24 23:40:14 -0400

    all files of 'introduction to python' are added

---
+++ 01. introduction to python/Q.15/welcome.py
@@ -0,0 +1,4 @@
+#usig a print("") keyword
+
+print("Hello, welcome to my portfolio")
+print("my name is Bhavesh , i am student of CodingGita , i am learning about Artificial Intelligent and Mchine Learning.")
