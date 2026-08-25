d784fd28bfe54b0fcf901498468fd5c5365998dd
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-24 23:03:49 -0400

    Q24 is solve

---
+++ 03. data_type & type_caste/Q24.py
@@ -0,0 +1,9 @@
+student_name="Rahul"
+student_age=18
+student_height=5.6
+student_result=None
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(student_result))
