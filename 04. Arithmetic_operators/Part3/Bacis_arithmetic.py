287422c8d15a4592e11499f3c4b34cd557b361e2
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-25 23:03:45 -0400

    bacis arithmetic file is added

---
+++ 04. Arithmetic_operators/Part3/Bacis_arithmetic.py
@@ -0,0 +1,25 @@
+a=3
+b=5
+
+print(a+b)
+#output is- 8
+
+print(a-b)
+#output is- -2
+
+print(a*b)
+#output is- 15
+
+print(a/b)
+#output is- 0.6
+
+print(a//b)
+#output is- 0
+
+print(a%b)
+#output is- 3
+
+print(a**b)
+#output is- 243
+
+
