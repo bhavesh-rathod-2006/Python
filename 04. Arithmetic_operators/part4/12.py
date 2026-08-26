7dfdb684ead7f5272466456418f41428442ee892
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-25 23:44:08 -0400

    12.py is added

---
+++ 04. Arithmetic_operators/part4/12.py
@@ -0,0 +1,15 @@
+print(10+5*2)
+print((10+5)*2)
+
+#first's output is 20
+#and second's output is 30
+
+print(20-10/2)
+print((20-10)/2)
+#1st ans is 5
+#2nd ans is 5
+
+print(2+3*4)
+print((2+3)*4)
+#1st ans is 14
+#2nd ans is 20
