7fc936e1d8cb4cce60ca03be1bc47c81ef82bd6a
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-25 23:11:18 -0400

    int_float file is added

---
+++ 04. Arithmetic_operators/Part3/int_float.py
@@ -0,0 +1,21 @@
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# results are respectivly 
+#7 , int
+#3 , int
+#10 , int
+#2.5 , float
+#2 , int
+#1 , int
+#25 , int
+
+
