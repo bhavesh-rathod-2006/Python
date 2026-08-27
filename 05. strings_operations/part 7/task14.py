8ac6b6168edecb6117d660572575d916a9c556dc
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-08-27 19:03:27 -0400

    the task 14 is done

---
+++ 05. strings_operations/part 7/task14.py
@@ -0,0 +1,21 @@
+name="Bhavesh"
+age="20"
+city="ahamdabad"
+programming_language="Python"
+
+print(name+" is "+age+" yers old and living at "+city+" his fav. subject is "+programming_language)
+      
+      
+#the output is-
+# Bhavesh is 20 yearsold and living at ahamdabad his fav. subject is Python
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
