ea49d73046414f849fc639249f50ec968344724b
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-02 22:08:38 -0400

    question is solved

---
+++ 06.Boolean_and_logical_expressions/28.py
@@ -0,0 +1,15 @@
+#For each value, identify whether it is truthy or falsy:
+
+#0,1,-5,"","Python",False,True,None
+
+print(bool(0))
+print(bool(1))
+print(bool(-5))
+print(bool(""))
+print(bool("python"))
+print(bool(False))
+print(bool(True))
+print(bool(None))
+
+
+# The results are False, True , True, False , true , False , True , False respectively
