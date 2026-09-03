5614051a5e082b499998251cd0d5a57cd9d745c1
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-02 22:15:59 -0400

    question is solved

---
+++ 06.Boolean_and_logical_expressions/30.py
@@ -0,0 +1,16 @@
+# Create a small Python program that demonstrates the difference between data type and truthiness using:
+# 0 ,1, "","python", False , None
+#For each value, identify:
+
+#1.Its data type.
+#2.Its Boolean interpretation.
+
+print(bool(0))    
+print(bool(1))
+print(bool(""))
+print(bool("python"))
+print(bool(False))
+print(bool(True))
+
+#The final ansers are false , True , False , True , False , true respectively .
+
