31e1a1467650ea3163201907e2dfc21d2e757f4b
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-04 18:20:16 -0400

    question is solved

---
+++ 07. input &  output/07.py
@@ -0,0 +1,5 @@
+name=input("Enter name : ")
+city=input("Enter city : ")
+collage=input("Enter your collage name : ")
+
+print(f"Your name is {name} and you are from {city} and your collage name is {collage}")
