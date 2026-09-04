8ec988cc3ee50d4e786c536997e7615a6aed8964
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-04 18:22:51 -0400

    question is solved

---
+++ 07. input &  output/30.py
@@ -0,0 +1,14 @@
+name=input("Enter your good name ")
+age=int(input("Enter your age "))
+height=float(input("Enter your height "))
+
+
+print(f"Your name is {name} , you are {age} years old and your height is {height:.2f}cm ")
+
+
+
+
+
+
+
+
