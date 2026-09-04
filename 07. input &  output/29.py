1a0bed081dbf297535ed74a0bc6c2365131257a0
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-04 18:22:39 -0400

    question is solved

---
+++ 07. input &  output/29.py
@@ -0,0 +1,6 @@
+name=input("Enter your name  ")
+age=int(input("Enter your age "))
+marks=float(input("Enter your exam's marks "))
+
+
+print(f"Your name is {name} , You are {age} year old and you achirve {marks} marks in your exams.")
