15e20e2207b5cf4fc1191976f4bdb9bc283c67e8
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-04 18:21:50 -0400

    question is solved

---
+++ 07. input &  output/22.py
@@ -0,0 +1,6 @@
+# inside the f-string the :.2f use is to count only two digits after decimal
+#for an example 
+# if i write price=input("write the price of product")
+#  print(f"{price:.2f}")
+#if user inout a value like 22.348533
+#the output will be come out 22.35
