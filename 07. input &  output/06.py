c42af42e5b8e4f164d582f05707569b88c3efff8
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-04 18:20:08 -0400

    question is solved

---
+++ 07. input &  output/06.py
@@ -0,0 +1,6 @@
+#Take first name and last name separately and display them together.
+
+first_name=input("Enter your first name here : ")
+last_name=input("Enter your last name here : ")
+
+print(first_name,last_name)
