72fa4c91479c5b5e9797c4941eae66ed718b7bcf
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-04 18:22:06 -0400

    question is solved

---
+++ 07. input &  output/23.py
@@ -0,0 +1,6 @@
+product_name=input("Enter product name")
+price=int(input("Enter the price of product"))
+quantity=int(input("write quantity of product"))
+total=price*quantity
+print(f"You purchased {quantity} {product_name}s and the price  of one {product_name} is {price} rupees , then the total amout is {total} ")
+
