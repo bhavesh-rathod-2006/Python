229e7e92ec0295ac5bb2109d8baccd888fdd10a9
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 21:59:55 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/19.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age "))
+
+if age >= 18 :
+    print("Adult")
+    if age<60:
+        print("You are albe to play ")
+    else:
+        print("Sorry,  you are too old to play ")    
+else:
+    print("You are not able to play ")        
+
