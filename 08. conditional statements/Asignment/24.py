bfc4ff858d70130dcf8f60a8ed4009a8c3b10b70
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 22:00:23 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/24.py
@@ -0,0 +1,7 @@
+number1=int(input("Enter first number "))
+number2=int(input("Enter second number "))
+
+if number1>10 and number2>10:
+    print("Both numbers are greater than 10 ")
+else:
+    print("Both numbers are not greater than 10 ")    
