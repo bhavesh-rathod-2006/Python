7b237ea0c3701c542890cde09b1a2b9ad18bf6b0
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 21:58:01 -0400

    question is sol5ed

---
+++ 08. conditional statements/Asignment/14.py
@@ -0,0 +1,10 @@
+marks=int(input("Enter your exam marks "))
+
+if marks>=90:
+    print("Excellent !!")
+elif marks>=75:
+    print("Good")
+elif marks>=40:
+    print("Pass")
+else:
+    print("Fail")            
