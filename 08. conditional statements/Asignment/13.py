5d3171a5ce6a91e786b0757d1dc182753fb6511e
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 21:57:53 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/13.py
@@ -0,0 +1,17 @@
+day=int(input("""Enter a number from below
+1. Monday
+2.Tuesday
+3.Wednestday
+4.Thursday
+5. Friday"""))
+
+if day==1 :
+    print("You choose Monday")
+elif day==2 :
+    print("You choose Tuesday")
+elif day==3 :
+    print("You choose Wednesday")
+elif day==4 :
+    print("You choose Thursday")
+else :
+    print("You choose Friday")           
