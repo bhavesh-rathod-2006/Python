8a25e62e124094435438e478d20158c9104b7a6c
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 22:00:04 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/20.py
@@ -0,0 +1,9 @@
+number=int(input("Enter a whole number "))
+
+if number != 0:
+    if number>0:
+        print("The input is possitive ")
+    else:
+        print("The input number is negetive ")    
+else:
+    print("The input number is zere ")        
