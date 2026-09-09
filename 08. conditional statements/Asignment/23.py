49c4c85cc6b74f1f0ddc17b9a53f5efdb4ecbe17
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 22:00:20 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/23.py
@@ -0,0 +1,18 @@
+age=int(input("Enter your age "))
+
+
+if age>=18:
+    has_id=(input("if you have student id than write true , if not then write false ")).strip().lower()
+
+    if has_id=="true":
+     print("You are eligible to enter in the collage ")
+    else:
+     print("You are not eligible to enter in the collage ")
+else:
+    print("You are minor and aren't eligible to enter in the collage ")  
+      
+
+
+
+  
+
