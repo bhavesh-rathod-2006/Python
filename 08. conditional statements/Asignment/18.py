13b03f235540396abbd41f304146829dc950a671
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 21:59:17 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/18.py
@@ -0,0 +1,9 @@
+number=int(input("Enter a whole number "))
+
+if number>=0:
+    if number<100:
+        print("Number is positive but less than 100")
+    else:
+        print("Number is possitive and greter than 100")    
+else:
+    print("The number is negetive")        
