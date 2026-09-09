d9bb4162606137eb4c472146637d4136fe7e7cd2
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 21:59:03 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/15.py
@@ -0,0 +1,14 @@
+number=int(input("Enter a number 1 to 5 "))
+
+if number==1:
+    print(f"Your input is {number}")
+elif number==2:
+     print(f"Your input is {number}")
+elif number==3:
+      print(f"Your input is {number}")       
+elif number==4:
+      print(f"Your input is {number}")
+elif  number==5 :
+      print(f"Your input is {number}")
+else:
+     print("YOU choose other")                    
