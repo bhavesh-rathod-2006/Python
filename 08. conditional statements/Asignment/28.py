8fc38d8daefe5d6b78009a44f5fffb89190c7629
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-08 22:00:46 -0400

    question is solved

---
+++ 08. conditional statements/Asignment/28.py
@@ -0,0 +1,5 @@
+number=int(input("Enter a number between 10 to 50 "))
+if number>=10 or number <=50:
+    print("the number is in the range of 10 and 50 ")
+else :
+    print("the number is not in the range of the 10 to 50 ")    
