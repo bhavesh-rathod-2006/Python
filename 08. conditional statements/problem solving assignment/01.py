f8ed3e80f9ccc93fe00f7da7fd7b363f1b24ec1c
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-09 23:42:27 -0400

    problem is solved

---
+++ 08. conditional statements/problem solving assignment/01.py
@@ -0,0 +1,11 @@
+number=int(input("Enter a whole number "))
+
+if number==0:
+    print("The input is zero ")
+else:
+    if number>0:
+        print("the input id possitive ")
+    else:
+        print("The input is negative ")   
+
+     
