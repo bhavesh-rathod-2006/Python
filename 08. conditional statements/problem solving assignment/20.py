dc8da1b007c7051aef011e9561c39eee69c74378
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-09 23:44:01 -0400

    problem is solved

---
+++ 08. conditional statements/problem solving assignment/20.py
@@ -0,0 +1,8 @@
+a=input("enter first side of triangle to form a valid t riangle ")
+b=input("enter first side of triangle to form a valid t riangle ")
+c=input("enter first side of triangle to form a valid t riangle ")
+
+if a+b>c and a+c>b and  b+c>a :
+    print("these three sides are form a valid triangle ")
+else:
+    print("these three sides are not form a valid triangle")    
