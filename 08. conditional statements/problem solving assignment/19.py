76bb3d0c83d29a538d21686166cb00fc2b0990cf
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-09 23:43:53 -0400

    problem is solved

---
+++ 08. conditional statements/problem solving assignment/19.py
@@ -0,0 +1,11 @@
+number=int(input("Enter a number "))
+if number<0:
+    print("Number is negetive ")
+elif 0<=number<=10:
+    print("Number is between 0 to 10 ")   
+elif 11<=number<=50:
+    print("Number is between 11 to 50 ")    
+elif 51<=number<=100:
+    print("Number is between 51 to 100 ")     
+else:
+    print("Number is above than 100 ")    
