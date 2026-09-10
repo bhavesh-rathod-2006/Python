1a3aca5202c005b840e7bb49c6c6f9980b68df51
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-09 23:44:19 -0400

    problem is solved

---
+++ 08. conditional statements/problem solving assignment/23.py
@@ -0,0 +1,9 @@
+username=input("Enter username ")
+if username=="admin": 
+    password=input("Enter password ")
+    if password=="python123":
+        print("Login Successful ")
+    else:
+        print("Wrong password ")    
+else:
+    print("User not found ")        
