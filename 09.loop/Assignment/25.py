488b8f47b02a6320176b09e0affbb611cbec2cd4
bhavesh-rathod-2006 <bhavesh.rathod.cg@gmail.com>
2026-09-10 22:38:13 -0400

    Problem is solved

---
+++ 09.loop/Assignment/25.py
@@ -0,0 +1,15 @@
+# string=input("Enter a string ").strip()
+
+# string2=string.upper().split()
+
+# for i in string:
+#     if i:
+#         print(i)
+
+
+string=input("Enter a string ").strip()
+sum=0
+for i in string:
+    if i==i.upper():
+        sum=sum+1
+print(sum)        
